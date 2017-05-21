ceeca11f7ed96d9adbd448cb72b907709f8c7d83
mynamekevin <mynamekevin@qq.com>
2017-05-21 11:39:45 +0800

    260. Single Number III

--- leetcode260.cpp
+++ leetcode260.cpp
@@ -5,6 +5,13 @@
 //  Created by 顾凯文 on 2017/5/21.
 //  Copyright © 2017年 顾凯文. All rights reserved.
 //
+//   Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+// For example:
+// Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+// Note:
+//     The order of the result is not important. So in the above example, [5, 3] is also correct.
+//     Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+
 //  思考两个问题，问题一：如何找到一个数的二进制某一位为1 ？问题二：几个数异或，一开始用什么数来异或第一个数呢？
 //  位运算符有： & | ^ ~ 分别代表 与、或、异或、非
 //  逻辑运算符： && || ! 分别代表 逻辑与、逻辑或、逻辑非
